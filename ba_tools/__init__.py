a5d0c36d12ac574087d4c4fabe7758ab51cce122
Artur Glavic <artur.glavic@psi.ch>
2022-12-06 16:49:38 +0100

    Initial commit

---
+++ ba_tools/__init__.py
@@ -0,0 +1,4 @@
+"""
+A support package for the BornAgain software to simplify creation of models and implementation
+of correct instrument resolution from instrument specific parameters.
+"""
---
+++ ba_tools/instruments/base.py
@@ -0,0 +1,4 @@
+"""
+Abstract base class for instrument definitions to be used to generate BornAgain simulation objects
+with correct detector and resolution.
+"""
